1f4208599771858b2fbff04245f0cd2f3ae3038a
David Husz <contact@davidhusz.com>
2020-10-17 06:12:55 +0200

    Improve file name generation

--- api/post.php
+++ api/post.php
@@ -1,16 +1,37 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Global variables
+    function get_unique_filename($extension, $original_filename = "") {
+        // return a file name which is unique in the contents directory
+        // (regardless of extension or original file name)
         $dir = "../contents";
         $date = date("U");
         
+        $id_collisions = 1;
+        while ($id_collisions > 0) {
+            // random five digit integer
+            $id = "$date-" . str_pad(rand(0, 99999), 5, 0, STR_PAD_LEFT);
+            $id_collisions = count(glob("$dir/$id.*"));
+            // maybe use `uniqid()` instead?
+        }
+        
+        if (!empty($original_filename)) {
+            // problematic characters will be removed from file name
+            $destination = "$dir/$id-"
+                           . preg_replace("/[^A-Za-z0-9_-]/", "", $original_filename)
+                           // allow dots too maybe?
+                           . ".$extension";
+        } else {
+            $destination = "$dir/$id.$extension";
+        }
+        
+        return $destination;
+    }
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Text upload
         if (isset($_POST["note"])) {
             $note = $_POST["note"];
-            $file = fopen("$dir/$date.txt", "x");
-            fwrite($file, "$note\n");
-            fclose($file);
-            // TODO: add random id to these file names as well
+            $dest = get_unique_filename("txt");
+            file_put_contents($dest, "$note\n");
         }
         
         // Files upload
@@ -25,13 +46,15 @@
                     $fsource = $contents["tmp_name"][$i];
                     
                     // File type & extension inference
-                    if (preg_match("/\.([a-z0-9]{1,5})$/", $fname, $match)) {
+                    if (preg_match("/(.+)\.([a-z0-9]{1,5})$/", $fname, $match)) {
                         // if the original file name has something that looks
                         // like a file extension, take that instead of trying
                         // to infer it
-                        $ext = $match[1];
+                        $fname = $match[1];
+                        $ext = $match[2];
                     } else {
-                        if ($ftype == "application/octet-stream") {
+                        $fname = "";
+                        if (empty($ftype) || $ftype == "application/octet-stream") {
                             // if file type is unknown, try to infer it
                             $ftype = mime_content_type($fsource);
                         }
@@ -49,29 +72,7 @@
                         }
                     }
                     
-                    /*
-                    // number of files with the same date identifier
-                    $samedate = count(glob("$dir/$date*.$ext"));
-                    
-                    if ($samedate == 0) {
-                        $fdest = "$dir/$date.$ext";
-                    } else {
-                        $fdest = "$dir/$date-" . ($samedate + 1) . ".$ext";
-                        if (file_exists("$dir/$date.$ext")) {
-                            rename("$dir/$date.$ext", "$dir/$date-1.$ext");
-                        }
-                    }
-                    */
-                    
-                    $fdest = $fsource;
-                    
-                    while (file_exists($fdest)) {
-                        // random five digit integer
-                        $random_id = str_pad(rand(0, 99999), 5, 0, STR_PAD_LEFT);
-                        $fdest = "$dir/$date-$random_id.$ext";
-                        // maybe use `uniqid()` instead?
-                    }
-                    
+                    $fdest = get_unique_filename($ext, $fname);
                     move_uploaded_file($fsource, $fdest);
                 }
             }
