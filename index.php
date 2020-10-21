8d35c991adfcff6567f26db0c12627af6b4a9ef6
David Husz <contact@davidhusz.com>
2020-10-21 02:29:21 +0200

    Add first version of 'new note' modal

--- index.php
+++ index.php
@@ -27,16 +27,20 @@
 		<link rel="icon" type="image/png" href="favicon.png">
 		<link rel="stylesheet" href="style.css">
 		<script type="text/javascript" src="controls.js"></script>
+		<script type="text/javascript" src="newnote.js"></script>
 		<title>Notes</title>
 	</head>
 	<body>
-	    <div class="wrapper">
+	    <div class="main-wrapper">
 	        <header>
                 <h1>Notes</h1>
+                <noscript>
+                    You have disabled JavaScript. Interactive components, such as the copy or the delete function, will not work correctly.
+                </noscript>
+                <div class="instructions">
+                    click <span class="clickable" onclick="showNoteForm()">here</span> or press <kbd>N</kbd> to create a new note
+                </div>
             </header>
-            <noscript>
-                You have disabled JavaScript. Interactive components, such as the copy or the delete function, will not work correctly.
-            </noscript>
             <main>
 	            <div class="note-list">
 	                <?php
@@ -46,6 +50,32 @@
 	                ?>
                 </div>
             </main>
+            <div class="new-note">
+                <div class="wrapper1-relative">
+                    <div class="shadow-background"></div>
+                    <div class="wrapper2-absolute">
+                        <div class="wrapper3-relative">
+                            <div class="modal">
+                                <form action="api/post.php" method="post">
+                                    <div>
+                                        <label for="form-contentbox">
+                                            Please enter a new note:
+                                        </label>
+                                        <span class="clickable close-button" onclick="hideNoteForm()">X</span>
+                                    </div>
+                                    <div class="contentbox">
+                                        <textarea id="form-contentbox" name="note"></textarea>
+                                    </div>
+                                    <div class="submit-button">
+                                        Press <kbd>Ctrl</kbd>+<kbd>Enter</kbd> or click
+                                        <button type="submit">Save</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="notification"></div>
         </div>
 	</body>
---
+++ newnote.js
@@ -0,0 +1,26 @@
+"use strict";
+
+window.addEventListener("keydown", function(event) {
+  if (event.key == "n") {
+    showNoteForm();
+  } else if (event.key == "Escape") {
+    hideNoteForm();
+  }
+});
+
+function showNoteForm() {
+    let noteForm = document.querySelector(".new-note");
+    noteForm.classList.add("active");
+    let contentBox = noteForm.querySelector("textarea");
+    contentBox.focus();
+    contentBox.addEventListener("keydown", function(event) {
+      if (event.ctrlKey && event.key == "Enter") {
+        noteForm.querySelector("form").submit();
+      }
+    });
+}
+
+function hideNoteForm() {
+  let noteForm = document.querySelector(".new-note");
+  noteForm.classList.remove("active");
+}
