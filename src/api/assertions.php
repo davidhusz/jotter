62db51dfddfcfe0f74fb0cd5e96342ee4658abe1
David Husz <contact@davidhusz.com>
2020-12-27 19:01:48 +0000

    Create function get_path_from_id

--- src/api/assertions.php
+++ src/api/assertions.php
@@ -17,4 +17,20 @@ function assert_http_parameters(...$params) {
         }
     }
 }
+
+function get_path_from_id($id) {
+    // TODO: assert that $id is a valid note id
+    $matches = glob("../contents/.*/$id*");  // `.*/` matches `./` as well
+    if (count($matches) == 1) {
+        return $matches[0];
+    } elseif (count($matches) == 0) {
+        http_response_code(404);  // 404 Not Found
+        echo "There is no note with id $id\n";
+        exit();
+    } else {
+        http_response_code(500);  // 500 Internal Server Error
+        echo "There are multiple notes with id $id\n";
+        exit();
+    }
+}
 ?>
